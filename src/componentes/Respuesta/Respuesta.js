78b83c4244bee52d06ebfe89bc1940aac3488867
Nicolas Martinez <martineznicolas551@gmail.com>
2022-06-13 18:56:13 -0300

    proyecto terminado

--- src/componentes/Respuesta/Respuesta.js
+++ src/componentes/Respuesta/Respuesta.js
@@ -2,34 +2,40 @@ import React, { useState } from "react";
 import Arrow from '../../imagenes/icon-arrow-down.svg';
 import './Respuesta.css'
 
-function Respuesta({ texto, textoRta }) {
+function Respuesta({ texto, textoRta, id }) {
 
-  const [estado, setEstado] = useState(false)
+  const [estado, setEstado] = useState(false);
 
-  function cambiarEstado() {
+  function cambiarEstado(id) {
     if(estado || !estado){
       setEstado(!estado)
       console.log(estado)
     }
 
+
     if(!estado){
-      document.querySelector('.respuesta').classList.add('seleccionado')
-      document.querySelector('.respuesta-wrapper-texto').classList.add('visible')
+      document.getElementById(`${id}`).classList.add('seleccionado');
+      document.querySelector(`[valor='${id}']`).classList.add('visible');
+      document.querySelector('.respuesta-arrow').classList.add('arrow');
+      console.log(id)
     }
     
     if(estado) {
-        document.querySelector('.respuesta').classList.remove('seleccionado')
-        document.querySelector('.respuesta-wrapper-texto').classList.remove('visible')
+        document.getElementById(`${id}`).classList.remove('seleccionado');
+        document.querySelector(`[valor='${id}']`).classList.remove('visible');
+        document.querySelector('.respuesta-arrow').classList.remove('arrow');
+        console.log(id)
     }
   }
 
+
   return(
-    <button className="respuesta" onClick={() => cambiarEstado()}>
+    <button className="respuesta" id={id} onClick={() => cambiarEstado(id)}>
       <p className="respuesta-texto">
         {texto}
       </p>
       <img src={Arrow} className="respuesta-arrow"/>
-      <p className="respuesta-wrapper-texto">
+      <p className="respuesta-wrapper-texto" valor={id}>
         {textoRta}
       </p>
     </button>
